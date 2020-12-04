148e9178e0ebe35a01747e200e63fbb98dd884f2
Kevin Garner <kevin@kgar.net>
2020-12-04 11:02:58 -0600

    starting on day 4 puzzle 2

--- src/day04.rs
+++ src/day04.rs
@@ -8,9 +8,42 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
+fn check_byr(s: String) -> bool {
+    true
+}
+
+fn check_iyr(s: String) -> bool {
+    true
+}
+
+fn check_eyr(s: String) -> bool {
+    true
+}
+
+fn check_hgt(s: String) -> bool {
+    true
+}
+
+fn check_hcl(s: String) -> bool {
+    true
+}
+
+fn check_ecl(s: String) -> bool {
+    true
+}
+
+fn check_pid(s: String) -> bool {
+    true
+}
+
 pub fn run() {
-    let mut checks = [("byr", false), ("iyr", false), ("eyr", false), ("hgt", false),
-                      ("hcl", false), ("ecl", false), ("pid", false)];
+    let mut checks = [("byr", false, false, check_byr as fn(String) -> bool),
+                      ("iyr", false, false, check_iyr as fn(String) -> bool),
+                      ("eyr", false, false, check_eyr as fn(String) -> bool),
+                      ("hgt", false, false, check_hgt as fn(String) -> bool),
+                      ("hcl", false, false, check_hcl as fn(String) -> bool),
+                      ("ecl", false, false, check_ecl as fn(String) -> bool),
+                      ("pid", false, false, check_pid as fn(String) -> bool)];
 
     let mut total_valid = 0;
     
@@ -27,8 +60,9 @@ pub fn run() {
                     for c in 0..checks.len() {
                         // if any are false, this will set all_good to false, and will stay there
                         // otherwise, if all are true, all_good will remain true
-                        all_good &= checks[c].1;
+                        all_good &= checks[c].1 && checks[c].2;
                         checks[c].1 = false;
+                        checks[c].2 = false;
                     }
                     total_valid += match all_good {
                         false => 0,
@@ -41,11 +75,15 @@ pub fn run() {
                     while let Some(test_char) = chars.next() {
                         if test_char == ':' as char {
                             let data = &parse[col-3..col];
+                            // find which piece of data this is and set that flag to true
                             for check in 0..checks.len() {
                                 if data == checks[check].0 {
                                     checks[check].1 = true;
+                                    //checks[check].2 = checks[check].3(data.to_string());
                                 }
                             }
+                            // validate the data is correct
+                            
                         }
                         col += 1;
                     }
