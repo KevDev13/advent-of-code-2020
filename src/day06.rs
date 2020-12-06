cd657d5353cae5fdd5bc1cd2509939341f5ccd8d
Kevin Garner <kevin@kgar.net>
2020-12-06 13:02:07 -0600

    day 6 puzzle 2 completed!

--- src/day06.rs
+++ src/day06.rs
@@ -10,34 +10,36 @@ where P: AsRef<Path>, {
 
 pub fn run() {
     let mut sum = 0;
-    let mut letters = [("a", false), ("b", false), ("c", false), ("d", false),
-                       ("e", false), ("f", false), ("g", false), ("h", false),
-                       ("i", false), ("j", false), ("k", false), ("l", false),
-                       ("m", false), ("n", false), ("o", false), ("p", false),
-                       ("q", false), ("r", false), ("s", false), ("t", false),
-                       ("u", false), ("v", false), ("w", false), ("x", false),
-                       ("y", false), ("z", false)];
+    let mut num_of_people = 0;
+    let mut letters = [("a", 0), ("b", 0), ("c", 0), ("d", 0),
+                       ("e", 0), ("f", 0), ("g", 0), ("h", 0),
+                       ("i", 0), ("j", 0), ("k", 0), ("l", 0),
+                       ("m", 0), ("n", 0), ("o", 0), ("p", 0),
+                       ("q", 0), ("r", 0), ("s", 0), ("t", 0),
+                       ("u", 0), ("v", 0), ("w", 0), ("x", 0),
+                       ("y", 0), ("z", 0)];
     if let Ok(lines) = read_lines("./input/day06.txt") {
         for line in lines {
             if let Ok(parse) = line {
                 if parse.len() == 0 {
                     for c in 0..letters.len() {
-                        sum += match letters[c].1 {
-                            true => 1,
-                            false => 0,
-                        };
-                        letters[c].1 = false;
+                        if letters[c].1 == num_of_people {
+                            sum += 1;
+                        }
+                        letters[c].1 = 0;
                     }
+                    num_of_people = 0;
                 }
                 else {
                     for c in 0..parse.len() {
                         let character = &parse[c..c+1];
                         for d in 0..letters.len() {
                             if character == letters[d].0 {
-                                letters[d].1 = true;
+                                letters[d].1 += 1;
                             }
                         }
                     }
+                    num_of_people += 1;
                 }
             }
         }
